23b8c344a893886ee4cee8a1b2a164b994cb2b38
ridhoajibx <ridhoajibx@gmail.com>
2021-04-18 16:39:49 +0700

    [ridho] - create env file

--- src/customHooks/useProduct.js
+++ src/customHooks/useProduct.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const url = process.env.REACT_APP_WINE_URL
+
 const useProduct = (identifier) => {
     const [product, setProduct] = useState([])
     const [loading, setLoading] = useState(false)
@@ -9,7 +11,7 @@ const useProduct = (identifier) => {
         const getProduct = async() => {
             setLoading(true);
             try {
-                let { data } = await axios.get(`https://zax5j10412.execute-api.ap-southeast-1.amazonaws.com/dev/api/product/${identifier}`)
+                let { data } = await axios.get(`${url}/${identifier}`)
                 setProduct(data.value);
                 setLoading(false);
             } catch (error) {
--- src/customHooks/useProducts.js
+++ src/customHooks/useProducts.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react'
 
+const url = process.env.REACT_APP_WINE_URL
+
 function useProducts(page) {
     const [products, setProducts] = useState([]);
     const [totalPage, setTotalPage] = useState(0)
     const [errors, setErrors] = useState('');
     const [loading, setLoading] = useState(false);
-
     useEffect(() => {
         const getProductList = async () => {
             setLoading(true);
             try {
-                const { data } = await axios.get(`https://zax5j10412.execute-api.ap-southeast-1.amazonaws.com/dev/api/product/list?page=${page}`)
+                const { data } = await axios.get(`${url}/list?page=${page}`)
                 setProducts(p => ([...p, ...data.value.products]));
                 setTotalPage(data.value.totalCount);
                 setLoading(false);
