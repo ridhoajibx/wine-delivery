c8a400432174fee63e3576a7ce362717f025a011
ridhoajibx <ridhoajibx@gmail.com>
2021-04-20 11:02:05 +0700

    [ridho] - create NotFound page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Products from '../views/Products';
 import Details from '../views/Details';
 import Carts from '../views/Carts';
+import NotFound from '../views/NotFound';
 
 export default function index() {
     return (
@@ -13,8 +14,13 @@ export default function index() {
                     <Redirect to="/products" />
                 </Route>
                 <Route exact path="/products" component={Products} />
-                <Route path="/products/:identifier" component={Details} />
-                <Route path="/your-cart" component={Carts} />
+                <Route exact path="/products/:identifier" component={Details} />
+                <Route exact path="/your-cart" component={Carts} />
+                <Route exact exact path="*" render={() => {
+                    return (
+                        <NotFound />
+                    )
+                }} />
             </Switch>
 
         </BrowserRouter>
